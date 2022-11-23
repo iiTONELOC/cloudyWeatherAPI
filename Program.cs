76127fb9f5eea47d5e576092701f064fe0ede318
iiTONELOC <anthonytropeano@me.com>
2022-11-23 17:56:27 -0500

    Have a basic API. More work is needed. But we are fetching current
    weather data from the OpenWeather OneCall API. We have two endpoints
    one for current weather and one for full current weather with the
    hourly, minutely, and daily forecasts.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using cloudyWeatherAPI.source.db;
+using cloudyWeatherAPI.source.utils;
+
+// load our envs
+// load the environment variables
+var root = Directory.GetCurrentDirectory();
+DotEnv.Load(Path.Combine(root, ".env").ToString());
+
+var weatherService = new WeatherService();
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+// returns the current, minutely, hourly, and daily weather data
+app.MapGet("/current", async (
+    [FromQuery(Name = "lat")] string? lat,
+    [FromQuery(Name = "lon")] string? lon
+    ) => await weatherService.GetCurrent(lat, lon, true));
+
+// only returns the current weather only
+app.MapGet("/current-full", async (
+    [FromQuery(Name = "lat")] string? lat,
+    [FromQuery(Name = "lon")] string? lon
+    ) => await weatherService.GetCurrent(lat, lon));
+
 
 app.Run();
---
+++ source/utils/DotEnv.cs
@@ -0,0 +1,38 @@
+﻿namespace cloudyWeatherAPI.source.utils
+{
+    using System;
+    using System.IO;
+
+
+    public static class DotEnv        
+    {
+        /// <summary>
+        /// Loads the environment variables from a .env file
+        /// </summary>
+        /// Adapted from https://dusted.codes/dotenv-in-dotnet
+        /// <param name="filePath"></param>
+        public static void Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(
+                    '=',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    continue;
+                var key = parts[0].Trim();
+                var value = parts[1].Trim();
+
+               
+        
+                Environment.SetEnvironmentVariable(key, value);
+
+
+            }
+        }
+    }
+}
